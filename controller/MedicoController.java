7a52e472bdc8ec43111984322ee4d9d5b286586c
Nathan Rodrigues dos Santos <97409681+NahNathan@users.noreply.github.com>
2023-04-21 14:09:12 -0300

    Corrigindo MedicoController e MedicoDAO

--- controller/MedicoController.java
+++ controller/MedicoController.java
@@ -5,71 +5,34 @@
 package controller;
 
 import java.util.Date;
+import model.MedicoDAO;
 import model.Medico;
-import model.Pessoa;
 
 /**
  *
  * @author nathan.santos
  */
 public class MedicoController {
-    private Medico[] medicos;
-    private int indice = 0;
+    private MedicoDAO dao = new MedicoDAO(1);
 
-    public MedicoController(int tamanho) {
-        this.medicos = new Medico[tamanho];
+    public void adicionarMedico(Medico medico) {
+        this.dao.adicionarMedico(medico);
+    }
+
+    public Medico buscarMedicoPorId(int id) {
+        return this.dao.buscarMedicoPorId(id);
     }
     
-    public void cadastrarMedico(String nome, String CRM, String cpf, String telefone, String especialidade) {
-        Pessoa pessoa = new Pessoa(0, nome, cpf, telefone, especialidade, especialidade, especialidade, especialidade, null, null);
-        Medico medico = new Medico(this.indice + 1, CRM, pessoa, especialidade, new Date(), new Date());
-        this.medicos[this.indice] = medico;
-        this.indice++;
+    public Medico[] listarPessoas() {
+        return this.dao.listarMedicos();
     }
     
-    public void atualizarMedico(int id, String nome, String cpf, String telefone, String especialidade) {
-        for (int i = 0; i < this.indice; i++) {
-            if (this.medicos[i].getId() == id) {
-                this.medicos[i].getPessoa().setNome(nome);
-                this.medicos[i].getPessoa().setCpf(cpf);
-                this.medicos[i].getPessoa().setTelefone(telefone);
-                this.medicos[i].setEspecialidade(especialidade);
-                this.medicos[i].setDataModificacao(new Date());
-                break;
-            }
-        }
+    public void atualizarMedico(Medico medico) {
+        this.dao.atualizarMedico(medico);
     }
 
-    public void excluirMedico(int id) {
-        for (int i = 0; i < this.indice; i++) {
-            if (this.medicos[i].getId() == id) {
-                for (int j = i; j < this.indice - 1; j++) {
-                    this.medicos[j] = this.medicos[j + 1];
-                }
-                this.medicos[this.indice - 1] = null;
-                this.indice--;
-                break;
-            }
-        }
-    }
-
-    public Medico buscarPorId(int id) {
-        for (int i = 0; i < this.indice; i++) {
-            if (this.medicos[i].getId() == id) {
-                return this.medicos[i];
-            }
-        }
-        return null;
-    }
-
-    public Medico[] listarMedicos() {
-        Medico[] resultado = new Medico[this.indice];
-        for (int i = 0; i < this.indice; i++) {
-            resultado[i] = this.medicos[i];
-        }
-        //System.arraycopy(this.medicos, 0, resultado, 0, this.indice);
-        //Ou assim
-        return resultado;
+    public void excluirrMedico(int id) {
+        this.dao.excluirMedico(id);
     }
     
 }
